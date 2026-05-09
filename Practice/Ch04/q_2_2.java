d31bd9b1c6ac080115cf58bb5e7208e385f87650
Jaewoo <tjw3901@gmail.com>
2026-05-10 02:41:40 +0900

    04-2-문제2 비트 쉬프트 연산자의 활용

---
+++ Practice/Ch04/q_2_2.java
@@ -0,0 +1,10 @@
+package com.test;
+
+public class q_2_2 {
+	public static void main(String[] args) {
+		int num = 15678;
+		
+		System.out.println((num >> 2) & 1);  
+		System.out.println((num >> 4) & 1);
+	}
+}
