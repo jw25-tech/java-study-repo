7d4c24b7c8718177d51492e30b6a37c7033a293d
Jaewoo <tjw3901@gmail.com>
2026-05-10 03:27:13 +0900

    for문의 활용

---
+++ Practice/Ch05/q_5_2.java
@@ -0,0 +1,11 @@
+package com.test;
+
+public class q_5_2 {
+	public static void main(String[] args) {
+		int num = 5;
+		
+		for(int i = 1; i < 10; i++) {
+			System.out.println(num + " X " + i + " = " + (num * i));
+		}
+	}
+}
