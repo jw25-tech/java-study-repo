f2fbbaa0537b6782c02d1d52d54c7c988cab32ad
Jaewoo <tjw3901@gmail.com>
2026-05-10 02:37:00 +0900

    04-1-문제4 연상의 출력과 활용

---
+++ Practice/Ch04/q_1_4.java
@@ -0,0 +1,23 @@
+package com.test;
+
+public class q_1_4 {
+	public static void main(String[] args) {
+		int num1 = (3 + 6);
+		int num2 = 9;
+		int num3 = 12;
+		System.out.println(num1);
+		System.out.println(num1 + num2);
+		System.out.println(num1 + num2 + num3);
+	}
+}
+
+/*
+int result = 3 + 6;
+System.out.println("3 + 6 = " + result);
+
+result += 9;
+System.out.println("3 + 6 + 9 = " + result);
+
+result += 12;
+System.out.println("3 + 6 + 9 + 12 = " + result);
+*/
