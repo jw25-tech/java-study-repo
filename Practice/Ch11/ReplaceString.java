2001d830e935ceeed2199b4c2845573a433053d5
Jaewoo <tjw3901@gmail.com>
2026-06-11 14:22:02 +0900

    String 클래스의 활용

--- Practice/Ch11/ReplaceString.java
+++ Practice/Ch11/ReplaceString.java
@@ -7,22 +7,25 @@ class ResidentNumber {
 		this.number = number;
 	}
 	
-	 public String Replace() {
+	 public String replaceString() {      // 메소드명 소문자로 수정
 	        String front = number.substring(0, 6);
 	        String back = number.substring(7);
 
 	        return front.concat(String.valueOf(' ')).concat(back);
 	    }
+
+		public String replaceStringBuilder() {
+		    StringBuilder stb = new StringBuilder(number);
+			stb.replace(6, 7, " ");
+			return stb.toString();
+		}
 }
 
-public class ReplaceHyphen {
-	
+public class ReplaceString {
 	 public static void main(String[] args) {
-
-	        ResidentNumber rn =
-	                new ResidentNumber("990925-1012999");
+	        ResidentNumber rn = new ResidentNumber("990925-1012999");
 
 	        System.out.println("990925-1012999");
-	        System.out.println(rn.Replace());
+	        System.out.println(rn.replaceString());
 	    }	
 }
