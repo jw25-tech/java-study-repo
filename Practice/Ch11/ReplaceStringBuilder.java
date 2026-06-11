d8663d117b79d106e97be38fe9fdf54586b7030c
Jaewoo <tjw3901@gmail.com>
2026-06-11 14:23:20 +0900

    StringBuilder 클래스의 활용

---
+++ Practice/Ch11/ReplaceStringBuilder.java
@@ -0,0 +1,10 @@
+package com.test;
+
+public class ReplaceStringBuilder {
+	public static void main(String[] args) {
+		ResidentNumber rn = new ResidentNumber("990925-1012999");
+		
+		System.out.println("990925-1012999");
+		System.out.println(rn.replaceStringBuilder());
+	}
+}
